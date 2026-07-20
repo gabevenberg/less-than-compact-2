133fdc72b148e9b3f38a1d39cb0a756d6dfef1c9
Gabe Venberg <gabevenberg@gmail.com>
2026-07-20 18:31:04 +0200

    added nix flake devshell.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "basic devshell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ] (system:
+        function {
+          pkgs = import nixpkgs {inherit system;};
+          inherit system;
+        });
+  in {
+    devShells = forAllSystems ({pkgs, ...}: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          just
+          packwiz
+        ];
+      };
+    });
+  };
+}
